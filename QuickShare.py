2471411dc0bc4a0797d8588e91d063404c941f1e
tanahiro2010 <herentongkegu087@gmail.com>
2024-05-30 20:08:28 +0900

    Update

--- QuickShare.py
+++ QuickShare.py
@@ -1,6 +1,45 @@
-import requests
 import json
-import uuid
-import hashlib
-import time
-from src import server
+from sys import argv
+from src.server import Server
+from src.client import Client
+
+def main():
+    # Promotion
+    ## Get Promotion text
+    with open(file='./data/Promotion', mode='r') as file:
+        Promotion = file.read()
+    print(Promotion)
+
+    argv_len = len(argv)
+    if argv_len == 1:
+        mode = input('Mode $')
+    else:
+        mode = argv[1]
+    mode = mode.lower()
+    print('Mode: {}'.format(mode))
+    if mode == 'server':
+        if argv_len == 1:
+            port = int(input('PORT $ '))
+        else:
+            port = int(argv[2])
+        soft_client = Server(port=port)
+        print('Server IP_Address: {}\nPORT: {}'.format(soft_client.host, soft_client.port))
+        soft_client.Begin_server()
+        return
+    elif mode == "send_file":
+        host = "127.0.0.1"
+        if argv_len == 1:
+            port = int(input('PORT $ '))
+        else:
+            port = int(argv[2])
+            if argv_len == 3:
+                host = argv[3]
+
+        soft_client = Client(port=port, host=host)
+        soft_client.connect()
+        soft_client.send(data=bytes("{'Title': 'Test'}", 'utf-8'))
+
+    return
+
+if __name__ == '__main__':
+    main()
--- src/server.py
+++ src/server.py
@@ -1,2 +1,30 @@
-import http.server
-import socketserver
+import json
+import socket
+import base64
+
+class Server():
+    def __init__(self, port):
+        self.port = port
+        with open(file='./data/config.json', mode='r') as f:
+            config = json.load(f)
+        self.host = config['host']
+        data = {
+            'host': self.host,
+            'port': self.port,
+        }
+
+        return
+
+    def Begin_server(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.host, self.port))
+            s.listen()
+            while True:
+                conn, addr = s.accept()
+                with conn:
+                    data = conn.recv(1024)
+                    data = str(data.decode())
+                    print(data)
+                    file_info = json.loads(data)
+                    print(file_info)
+        return
